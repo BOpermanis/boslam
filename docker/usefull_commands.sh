6d429533ff3ef00ed12c0a4b85edc3795dc1c05a
bruno <bruno.opermanis@gmail.com>
2020-04-12 22:17:08 +0300

    updated usefull_commands.sh

--- docker/usefull_commands.sh
+++ docker/usefull_commands.sh
@@ -2,9 +2,9 @@
 
 docker run -it pyorbslam /bin/bash
 
-docker run --name pyorbslam0 -p 2222:2222 -itd \
+docker run --name boslam -p 2222:2222 -itd \
            -e USER=bruno -e PASSWORD=www \
            -v slam_data:/home/slam_data \
            --privileged=true \
            --device=/dev:/dev \
-           pyorbslam
+           myslam
