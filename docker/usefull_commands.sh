05690e97ccda6f56da0640f3aec39debde908e5b
bruno <bruno.opermanis@gmail.com>
2020-04-13 13:08:54 +0300

    added lib from pydbow3

--- docker/usefull_commands.sh
+++ docker/usefull_commands.sh
@@ -2,7 +2,7 @@
 
 docker run -it boslam /bin/bash
 
-docker run --name boslam -p 2222:2222 -itd \
+docker run --name boslam0 -p 2222:2222 -itd \
            -e USER=bruno -e PASSWORD=www \
            -v slam_data:/home/slam_data \
            --privileged=true \
